00a44f2b0c1d360908204c19c6659831644214b0
Eon S. Jeon <esjeon@hyunmu.am>
2019-01-16 14:29:42 +0900

    update years in copyright notice

--- src/qt.d.ts
+++ src/qt.d.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Eon S. Jeon <esjeon@hyunmu.am>
+// Copyright (c) 2018-2019 Eon S. Jeon <esjeon@hyunmu.am>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
