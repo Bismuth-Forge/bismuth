0192f16a65631ef9a99350b3ed1ff8da0b655bb4
Eon S. Jeon <esjeon@hyunmu.am>
2019-02-22 20:56:15 +0900

    refactor: split `Rect` to a new file

--- src/common/rect.ts
+++ src/common/rect.ts
@@ -18,44 +18,47 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-enum UserInput {
-    Left,
-    Right,
-    Up,
-    Down,
+class Rect {
+    public readonly height: number;
+    public readonly width: number;
+    public readonly x: number;
+    public readonly y: number;
 
-    ShiftLeft,
-    ShiftRight,
-    ShiftUp,
-    ShiftDown,
-
-    Increase,
-    Decrease,
-
-    Float,
-    SetMaster,
-    CycleLayout,
-    SetLayout,
-}
-
-function clip(value: number, min: number, max: number): number {
-    if (value < min)
-        return min;
-    if (value > max)
-        return max;
-    return value;
-}
-
-function slide(value: number, step: number): number {
-    if (step === 0)
-        return value;
-    return Math.floor(value / step + 1.000001) * step;
-}
-
-function matchWords(str: string, words: string[]): number {
-    for (let i = 0; i < words.length; i++) {
-        if (str.indexOf(words[i]) >= 0)
-            return i;
+    constructor(x: number, y: number, w: number, h: number) {
+        this.height = h;
+        this.width = w;
+        this.x = x;
+        this.y = y;
     }
-    return -1;
+
+    public equals(other: Rect) {
+        return (
+            this.x === other.x &&
+            this.y === other.y &&
+            this.width === other.width &&
+            this.height === other.height
+        );
+    }
+
+    public gap(left: number, right: number, top: number, bottom: number): Rect {
+        return new Rect(
+            this.x + left,
+            this.y + top,
+            this.width - (left + right),
+            this.height - (top + bottom),
+        );
+    }
+
+    public subtract(other: Rect) {
+        return new Rect(
+            this.x - other.x,
+            this.y - other.y,
+            this.width - other.width,
+            this.height - other.height,
+        );
+    }
+
+    public toString(): string {
+        return "Rect(" + [this.x, this.y, this.width, this.height].join(", ") + ")";
+    }
 }
