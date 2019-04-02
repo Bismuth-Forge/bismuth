0287ceac04f02b6404674e13c69fbc85140cdeec
Eon S. Jeon <esjeon@hyunmu.am>
2019-04-03 07:54:03 +0900

    declare `setTimeout` in KWin driver

--- src/driver/kwin/kwinsettimeout.ts
+++ src/driver/kwin/kwinsettimeout.ts
@@ -48,3 +48,11 @@ class KWinTimerPool {
 function KWinSetTimeout(func: () => void, timeout: number) {
     KWinTimerPool.instance.setTimeout(func, timeout);
 }
+
+declare var setTimeout: any;
+try {
+    if (!setTimeout)
+        setTimeout = KWinSetTimeout;
+} catch (e) {
+    /* ignore */
+}
