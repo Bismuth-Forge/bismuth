c2bf027e13ea79949c5709f4511fb941e573d8a9
Eon S. Jeon <esjeon@hyunmu.am>
2020-07-07 21:41:36 +0900

    implement RotateLayoutPart
    RotateLayoutPart allows rotating its child LayoutPart (and also supports
    layout adjustment).
    
    Note that "rotation" here isn't real degree-based rotation. This is an
    intentional decision to preserve stack-growing direction (top-to-bottom,
    left-to-right).

--- src/layouts/layoutpart.ts
+++ src/layouts/layoutpart.ts
@@ -107,3 +107,58 @@ class StackLayoutPart implements ILayoutPart {
         return LayoutUtils.splitAreaWeighted(area, weights, this.gap);
     }
 }
+
+class RotateLayoutPart implements ILayoutPart {
+    constructor(
+        public child: ILayoutPart,
+        public direction: 0 | 90 | 180 | 270 = 0,
+    ) {
+    }
+
+    public adjust(area: Rect, tiles: Window[], basis: Window, delta: RectDelta): void {
+        switch (this.direction) {
+            case 0  : break;
+            case 90 :
+                area = new Rect(area.y, area.x, area.height, area.width);
+                delta = new RectDelta(delta.south, delta.north, delta.east, delta.west); break;
+            case 180:
+                delta = new RectDelta(delta.west, delta.east, delta.south, delta.north); break;
+            case 270:
+                area = new Rect(area.y, area.x, area.height, area.width);
+                delta = new RectDelta(delta.north, delta.south, delta.east, delta.west); break;
+        }
+
+        this.child.adjust(area, tiles, basis, delta);
+    }
+
+    public apply(area: Rect, tiles: Window[]): Rect[] {
+        switch (this.direction) {
+            case 0  : break;
+            case 90 : area = new Rect(area.y, area.x, area.height, area.width); break;
+            case 180: break;
+            case 270: area = new Rect(area.y, area.x, area.height, area.width); break;
+        }
+
+        const childResult = this.child.apply(area, tiles);
+
+        switch (this.direction) {
+            case 0:
+                return childResult;
+            case 90:
+                return childResult.map((g) =>
+                    new Rect(g.y, g.x, g.height, g.width));
+            case 180:
+                return childResult.map((g) => {
+                    const rx = g.x - area.x;
+                    const newX = area.x + area.width - (rx + g.width);
+                    return new Rect(newX, g.y, g.width, g.height)
+                });
+            case 270:
+                return childResult.map((g) => {
+                    const rx = g.x - area.x;
+                    const newY = area.x + area.width - (rx + g.width);
+                    return new Rect(g.y, newY, g.height, g.width)
+                });
+        }
+    }
+}
