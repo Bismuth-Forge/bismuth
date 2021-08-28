06f0e7edd3af82518a728646083dfe1f697e1884
Mikhail Zolotukhin <mail@genda.life>
2021-08-28 15:31:45 +0300

    fix: install from correct path

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Display info and upgrade/install script
-KWINPKG_FILE="${npm_package_name}-${npm_package_version}.kwinscript"
+KWINPKG_FILE="$npm_package_config_build_dir/${npm_package_name}-${npm_package_version}.kwinscript"
 plasmapkg2 -t kwinscript -s $npm_package_name && plasmapkg2 -u "$KWINPKG_FILE" || plasmapkg2 -i "$KWINPKG_FILE"
 
