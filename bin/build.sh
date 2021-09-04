5d4a33c17be2c20c72f2f75288366ba40e61089a
Mikhail Zolotukhin <mail@genda.life>
2021-09-05 01:14:42 +0300

    build: fix linting errors in bash scripts

--- bin/build.sh
+++ bin/build.sh
@@ -7,30 +7,30 @@
 set -e
 
 # Make necessary directories
-mkdir -p $npm_package_config_build_dir/contents/code
-mkdir -p $npm_package_config_build_dir/contents/config
-mkdir -p $npm_package_config_build_dir/contents/ui
+mkdir -p "${npm_package_config_build_dir:=build}/contents/code"
+mkdir -p "$npm_package_config_build_dir/contents/config"
+mkdir -p "$npm_package_config_build_dir/contents/ui"
 
 # Compile source into the form usable from QML
 echo "Compiling typescript..."
-npx tsc --outDir $npm_package_config_build_dir/contents/code/
+npx tsc --outDir "$npm_package_config_build_dir/contents/code/"
 
 # Rename all js files to mjs, because TypeScript cannot do that (https://github.com/Microsoft/TypeScript/issues/18442)
 # We need to to that in order to use Javascripts modules from Qt
-find $npm_package_config_build_dir/contents/code/ -name "*.js" -exec bash -c 'mv "$1" "${1%.js}".mjs' - '{}' \;
+find "$npm_package_config_build_dir/contents/code/" -name "*.js" -exec bash -c 'mv "$1" "${1%.js}".mjs' - '{}' \;
 
 # Fix the import statements (replace .js to .mjs, or add .mjs extention)
-find $npm_package_config_build_dir/contents/code/ -name "*.mjs" -exec sed -i '/^import/s/\(\.js\)*";*$/.mjs";/g' {} +
+find "$npm_package_config_build_dir/contents/code/" -name "*.mjs" -exec sed -i '/^import/s/\(\.js\)*";*$/.mjs";/g' {} +
 
 # Copy resources to the build directory with correct paths
-cp -v res/config.ui $npm_package_config_build_dir/contents/ui/config.ui
-cp -v res/popup.qml $npm_package_config_build_dir/contents/ui/popup.qml
-cp -v res/main.qml $npm_package_config_build_dir/contents/ui/main.qml
-cp -v res/config.xml $npm_package_config_build_dir/contents/config/main.xml
+cp -v res/config.ui "$npm_package_config_build_dir/contents/ui/config.ui"
+cp -v res/popup.qml "$npm_package_config_build_dir/contents/ui/popup.qml"
+cp -v res/main.qml "$npm_package_config_build_dir/contents/ui/main.qml"
+cp -v res/config.xml "$npm_package_config_build_dir/contents/config/main.xml"
 
 # Copy and update metadata
-METADATA_FILE=$npm_package_config_build_dir/metadata.desktop
+METADATA_FILE="$npm_package_config_build_dir/metadata.desktop"
 
-cp -v res/metadata.desktop $METADATA_FILE
-sed -i "s/\$VER/$npm_package_version/" $METADATA_FILE
+cp -v res/metadata.desktop "$METADATA_FILE"
+sed -i "s/\$VER/${npm_package_version:-1.0}/" "$METADATA_FILE"
 
--- bin/package.sh
+++ bin/package.sh
@@ -6,10 +6,10 @@
 
 set -e
 
-KWINPKG_FILE="$npm_package_config_build_dir/${npm_package_name}-${npm_package_version}.kwinscript"
+KWINPKG_FILE="${npm_package_config_build_dir:=build}/${npm_package_name:-Bismuth}-${npm_package_version:-1.0}.kwinscript"
 
 # Remove old archive
 rm -f "$KWINPKG_FILE"
 
 # Create new installable package
-7z a -tzip "$KWINPKG_FILE" ./$npm_package_config_build_dir/contents/ ./$npm_package_config_build_dir/metadata.desktop
+7z a -tzip "$KWINPKG_FILE" ./${npm_package_config_build_dir}/contents/ ./${npm_package_config_build_dir}/metadata.desktop
