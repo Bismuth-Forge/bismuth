ff3375314b8cf3863d39875cb8751469a71977d6
Mikhail Zolotukhin <mail@genda.life>
2021-11-01 21:06:08 +0300

    fix!: remove old config button from KWin scripts page
    Configuration is now done via Bismuth KCM.

---
+++ src/kcm/kconf_update/old_conf_ui.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: 2021 Mikhail Zolotukhin <mail@genda.life>
+# SPDX-License-Identifier: MIT
+
+# Remove symlink for legacy config ui
+rm ~/.local/share/kservices5/bismuth.desktop
