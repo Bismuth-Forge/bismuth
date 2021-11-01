e73cbce4c8af5ab308634972409b4f7f87e95085
Mikhail Zolotukhin <mail@genda.life>
2021-11-02 01:24:14 +0300

    feat: reload script after applying configuraton

--- src/kcm/bismuth.cpp
+++ src/kcm/bismuth.cpp
@@ -6,10 +6,15 @@
 
 #include "bismuth.h"
 #include "bismuth_config.h"
+#include "kwin_interface.h"
+
+#include <QDBusInterface>
+#include <QDebug>
 
 #include <KAboutData>
 #include <KLocalizedString>
 #include <KPluginFactory>
+#include <qstringliteral.h>
 
 K_PLUGIN_CLASS_WITH_JSON(BismuthSettings, "metadata.json")
 
@@ -17,16 +22,14 @@ BismuthSettings::BismuthSettings(QObject *parent, const QVariantList &args)
     : KQuickAddons::ManagedConfigModule(parent, args)
     , m_config(new Bismuth::Config(this))
 {
-    KAboutData *aboutData =
-        new KAboutData(QStringLiteral("kcm_bismuth"),
-                       i18nc("@title", "Window Tiling"),
-                       QStringLiteral("1.0"),
-                       QStringLiteral(""),
-                       KAboutLicense::LicenseKey::Custom,
-                       i18nc("@info:credit", "Copyright 2021 Mikhail Zolotukhin <mail@genda.life>"));
+    KAboutData *aboutData = new KAboutData(QStringLiteral("kcm_bismuth"),
+                                           i18nc("@title", "Window Tiling"),
+                                           QStringLiteral("1.0"),
+                                           QStringLiteral(""),
+                                           KAboutLicense::LicenseKey::Custom,
+                                           i18nc("@info:credit", "Copyright 2021 Mikhail Zolotukhin <mail@genda.life>"));
 
-    aboutData->addAuthor(
-        i18nc("@info:credit", "Author"), i18nc("@info:credit", "Author"), QStringLiteral("author@domain.com"));
+    aboutData->addAuthor(i18nc("@info:credit", "Author"), i18nc("@info:credit", "Author"), QStringLiteral("author@domain.com"));
 
     setAboutData(aboutData);
     setButtons(Help | Apply | Default);
@@ -37,6 +40,22 @@ BismuthSettings::BismuthSettings(QObject *parent, const QVariantList &args)
 Bismuth::Config *BismuthSettings::config() const
 {
     return m_config;
+}
+
+void BismuthSettings::save()
+{
+    KQuickAddons::ManagedConfigModule::save();
+    reloadKWinScript();
+}
+
+void BismuthSettings::reloadKWinScript() const
+{
+    OrgKdeKwinScriptingInterface kwinInterface(QStringLiteral("org.kde.KWin"), QStringLiteral("/Scripting"), QDBusConnection::sessionBus());
+
+    // Unload Bismuth, so that it can reload its configuration
+    kwinInterface.unloadScript(QStringLiteral("bismuth")).waitForFinished(); // Sync call
+    // Load unloaded scripts
+    kwinInterface.start(); // Async call
 }
 
 #include "bismuth.moc"
