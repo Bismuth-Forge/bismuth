4e4ccdd49bd84bbd8b97898be37b4a934e2e2b0e
Mikhail Zolotukhin <mail@genda.life>
2021-11-01 19:09:12 +0300

    chore: move kwinscript to its own dir

---
+++ scripts/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: 2021 Mikhail Zolotukhin <mail@genda.life>
+# SPDX-License-Identifier: MIT
+
+set -e
+
+
+echo "🏗️ Building KWin Script..."
+
+KWINSCRIPT_SOURCEDIR="src/kwinscript"
+KWINSCRIPT_BUILDDIR="build/kwinscript"
+
+# Make necessary directories
+mkdir -p "$KWINSCRIPT_BUILDDIR/contents/code"
+
+echo "👮 Checking using TS Compiler..."
+npx tsc --noEmit --incremental
+
+echo "🎁 Bundling using esbuild..."
+npx esbuild \
+  --bundle "$KWINSCRIPT_SOURCEDIR/index.ts" \
+  --outfile="$KWINSCRIPT_BUILDDIR/contents/code/index.mjs" \
+  --format=esm \
+  --platform=neutral
+
+# Copy resources to the build directory with correct paths
+echo "📑 Preparing UI and metadata files..."
+cp -r "$KWINSCRIPT_SOURCEDIR/res/ui" "$KWINSCRIPT_BUILDDIR/contents"
+cp -r "$KWINSCRIPT_SOURCEDIR/res/config" "$KWINSCRIPT_BUILDDIR/contents"
+
+# Copy and update metadata
+METADATA_FILE="$KWINSCRIPT_BUILDDIR/metadata.desktop"
+
+cp "$KWINSCRIPT_SOURCEDIR/res/metadata.desktop" "$METADATA_FILE"
+sed -i "s/\$VER/${npm_package_version:-1.0}/" "$METADATA_FILE"
+
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
 # SPDX-FileCopyrightText: 2021 Mikhail Zolotukhin <mail@genda.life>
-# 
 # SPDX-License-Identifier: MIT
 
 set -e
 
 # Remove build directory and package-lock
-rm -vrf "${npm_package_config_build_dir:=build}"
+rm -vrf "build"
 rm -vf package-lock.json
 rm -vrf node_modules/
 
