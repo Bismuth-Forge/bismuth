b82e0b270698539a510a617c3736d60d1450359f
Mikhail Zolotukhin <mail@genda.life>
2021-11-09 23:40:29 +0300

    feat: make script installation directory global

---
+++ src/kwinscript/kconf_update/local_install_to_global.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: 2021 Mikhail Zolotukhin <mail@genda.life>
+# SPDX-License-Identifier: MIT
+
+# Remove local installation of the script.
+# It will be substituted with the global installation.
+rm -r ~/.local/share/kwin/scripts/bismuth
