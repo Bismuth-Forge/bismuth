25fe937774657321146c0d2dc3db31ebb58afe33
Mikhail Zolotukhin <mail@gikari.com>
2022-08-01 20:28:21 +0300

    ci: fix qt5 dependecy

--- .github/scripts/sysdep-install.sh
+++ .github/scripts/sysdep-install.sh
@@ -5,9 +5,10 @@
 
 set -e
 
+apt-get update && apt-get upgrade -y
 apt-get update && apt-get install -y \
   zip cmake ninja-build extra-cmake-modules kirigami2-dev \
   libkf5config-dev libkf5configwidgets-dev libkf5coreaddons-dev \
   libkf5declarative-dev libkf5i18n-dev libkf5kcmutils-dev \
   libkdecorations2-dev libqt5svg5-dev qml-module-qtquick* qtbase5-dev \
-  qtdeclarative5-dev qtquickcontrols2-5-dev g++
+  qtdeclarative5-dev qtquickcontrols2-5-dev g++ libqt5gui5
